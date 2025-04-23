fcc080d4bf67fdedda736fde427835df9bf90e6c
Luis-coelho30 <luis.coelho.761@gmail.com>
2025-04-23 16:16:34 -0300

    Finalizando a lógica de votação em candidatos

---
+++ voting-app/src/main/java/domain/ElectionRepository.java
@@ -0,0 +1,12 @@
+package domain;
+
+import java.util.List;
+
+public interface ElectionRepository {
+
+    List<Election> findAll();
+
+    Election findById(String id);
+
+    void vote(String id, Candidate candidate);
+}
